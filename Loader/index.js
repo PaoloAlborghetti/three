36dea8d6b6440e2babc707885993be939998983d
paolo_albo <paolo.alborghetti@gmail.com>
2022-09-27 08:11:30 +1000

    boiler plate for glb loader

---
+++ Loader/index.js
@@ -0,0 +1,167 @@
+import {
+  Scene,
+  BoxGeometry,
+  MeshBasicMaterial,
+  Mesh,
+  PerspectiveCamera,
+  WebGLRenderer,
+  Vector2,
+  Vector3,
+  Vector4,
+  Quaternion,
+  Matrix4,
+  Spherical,
+  Box3,
+  Sphere,
+  Raycaster,
+  MathUtils,
+  MOUSE,
+  Clock,
+  MeshLambertMaterial,
+  DirectionalLight,
+  SphereGeometry,
+  MeshPhongMaterial,
+  TextureLoader,
+  LoopPingPong,
+  LoadingManager,
+  HemisphereLight,
+  AmbientLight,
+  AxesHelper,
+  GridHelper,
+  Color,
+  EdgesGeometry,
+  LineBasicMaterial,
+  LineSegments,
+} from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import CameraControls from "camera-controls";
+import GUI from 'three/examples/jsm/libs/lil-gui.module.min.js';
+import gsap from 'gsap';
+
+import{GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+//1 scene
+
+const scene = new Scene();
+const canvas = document.getElementById("three-canvas");
+
+//helpers
+
+const axes = new AxesHelper(1);
+axes.material.depthTest=false;
+axes.renderOrder = 2;
+scene.add(axes);
+
+
+const grid = new GridHelper();
+grid.renderOrder = 1;
+scene.add(grid);
+
+//2 the object
+
+const loader = new GLTFLoader();
+loader.load('./link/police_station.glb',
+
+(gltf) => {
+  scene.add(gltf.scene);
+},
+
+(progress) => {
+  console.log(progress);
+},
+
+(error) => {
+  console.log(error);
+},
+
+);
+
+
+//video at 
+// https://youtu.be/NS2Wn8Z0WbA?t=473
+
+// ------------------------------------------create camera
+
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+const camera = new PerspectiveCamera(
+  75,
+  canvas.clientWidth / canvas.clientHeight
+);
+camera.position.z = 3;
+camera.position.x = 3;
+camera.position.y = 2;
+scene.add(camera);
+
+// --------------------------------------------three renderer
+const renderer = new WebGLRenderer({ canvas });
+const pixelRat = Math.min(window.devicePixelRatio, 2);
+renderer.setPixelRatio(pixelRat);
+renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
+renderer.setClearColor(0x555555,1);
+
+
+
+//Lights
+
+const dirLight = new DirectionalLight();
+dirLight.position.set(3,2,1).normalize();
+scene.add(dirLight);
+
+// const ambLight = new AmbientLight( 0xFFFFFF,0.5);
+// scene.add(ambLight);
+
+const hemisphereLight = new HemisphereLight(0x70c2b4,0xff1453);
+scene.add(hemisphereLight);
+
+//Camera
+
+window.addEventListener("resize", () => {
+  camera.aspect = canvas.clientWidth / canvas.clientHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
+});
+
+//Camera controls via camera-control lb
+
+const subsetOfTHREE = {
+  MOUSE,
+  Vector2,
+  Vector3,
+  Vector4,
+  Quaternion,
+  Matrix4,
+  Spherical,
+  Box3,
+  Sphere,
+  Raycaster,
+  MathUtils: {
+    DEG2RAD: MathUtils.DEG2RAD,
+    clamp: MathUtils.clamp,
+  },
+};
+
+CameraControls.install({ THREE: subsetOfTHREE });
+const clock = new Clock();
+const cameraControls = new CameraControls(camera, canvas);
+
+//not working camera target ??? 
+//cameraControls.setTarget(box.x,box.y,box.z,false);
+
+//animate
+
+function animate() {
+ 
+
+  const delta = clock.getDelta();
+  cameraControls.update(delta);
+  renderer.render(scene, camera);
+
+  requestAnimationFrame(animate);
+}
+
+animate();
+
